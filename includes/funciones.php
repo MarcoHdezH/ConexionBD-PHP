bbc314872ce5a73ac3ddc1425fe5ee891d9685f7
Marco Hernandez <marco7245@hotmail.com>
2023-01-31 19:04:04 -0600

    Acceso a BD Local

---
+++ includes/funciones.php
@@ -0,0 +1,22 @@
+<?php
+function obtenerServicios(){
+
+    try{
+
+        require 'database.php';
+
+        $sql = "SELECT * FROM servicios;";
+
+        $consulta = mysqli_query($db,$sql);
+
+        echo "<pre>";
+        var_dump(mysqli_fetch_all($consulta));
+        echo "</pre>";
+
+    }catch(\Throwable $th){
+        var_dump($th);
+    }
+
+}
+
+obtenerServicios();
